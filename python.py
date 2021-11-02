0f4e7e4332f0a8be05704fc40d6394489d5abdb4
Sathvik Udupa <sathvikudupa66@gmail.com>
2021-11-02 10:00:30 +0530

    Update python.py

--- python.py
+++ python.py
@@ -3,3 +3,15 @@ Specify gpu ids to be utilised.
 Helpful while training multi gpu models on specific gpus/ pytorch pin memory True on gpu id != 0
 '''
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+'''
+read yaml file as attrdict
+'''
+from attrdict import AttrDict
+import yaml
+
+def read_yaml(yamlFile):
+    with open(yamlFile) as f:
+        config = yaml.load(f, Loader=yaml.SafeLoader)
+        cfg = AttrDict(config)
+    return cfg
