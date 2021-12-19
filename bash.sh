b3142fafba8b89ad75b2d831606c53bc3515d5dd
Sathvik Udupa <sathvikudupa66@gmail.com>
2021-12-19 15:07:45 +0530

    Update and rename bash.md to bash.sh

--- bash.sh
+++ bash.sh
@@ -1,18 +1,18 @@
-separate text in file by delimiter
-```
+#separate text in file by delimiter
+
 cat filename.txt | cut -d "|" -f 1 >> outfilename.txt
-```
 
-fasttext classifier
-```
+
+#fasttext classifier
+
 ./fasttext supervised -input . -output mr_4class -epoch 50 -wordNgrams 2
-```
 
-Basename from path
-```
+
+#Basename from path
+
 path='folder/other_folder/file.js'
 
 base=${path##*/}
 echo ${base%.*}
-```
+
 
